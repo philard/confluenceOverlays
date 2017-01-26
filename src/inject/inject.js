98a0f6b4eb7ed86dbe4a12dff35310b537d6aad6
Philip John Ardley <Philip_John_Ardley@epam.com>
2017-01-26 10:08:04 +0800

    - POC of JIRA status count. - More simple stats.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,21 +3,57 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+    function getClosest(selector, element) {
+      let res = element;
+      while (res && !res.matches(selector)) {
+        res = res.parentNode;
+      }
+      return res;
+    }
+
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
 
-		let testres = Array.prototype.filter.call(document.querySelectorAll(
-		'thead div.tablesorter-header-inner'), 
-		(el) => el.innerText == "Test Result")[0];
 
-		let parentTable = testres;
-		while (parentTable && !parentTable.matches('table')) {
-			parentTable = parentTable.parentNode;
-		}
-		
+		let testResultsHeader = Array.prototype.filter.call(document.querySelectorAll(
+  		'thead div.tablesorter-header-inner'),
+  		(el) => el.innerText == "Test Result")[0];
+
+    let headers = Array.prototype.slice.call(
+      getClosest('tr', testResultsHeader).childNodes);
+
+    let parentTable = getClosest('table', testResultsHeader);
+
+    let jIRALinks = parentTable.querySelectorAll('a.jira-issue-key');
+    let jIRAInfos = [];
+    for(i =0; jIRALinks.length > i; i++) {
+      let td = getClosest('td', jIRALinks[0]);
+      let trIndex = Array.prototype.indexOf.call(td.parentNode.childNodes, td);
+      jIRAInfo = {
+        'key': jIRALinks[0].attributes['href'].value,
+        'status': jIRALinks[0].innerText,
+        'trIndex': trIndex,
+        'headerEl': headers[trIndex]
+      };
+      jIRAInfos.push(jIRAInfo);
+    }
+
+    let columnsWithJiraInfo = headers.map((header, index) => {
+      return {
+        'index': index,
+        'headerEl': header,
+        'jIRAInfos': jIRAInfos.filter((info) => info.trIndex == index)
+      }
+    });
+
+    columnsWithJiraInfo =
+      columnsWithJiraInfo.filter((column) => column.jIRAInfos.length > 0);
+
+
+
 		var row = parentTable.insertRow(1);
 		var cell1 = row.insertCell(0);
 		row.insertCell();
@@ -26,8 +62,6 @@ chrome.extension.sendMessage({}, function(response) {
 		var cell4 = row.insertCell();
 		cell1.innerHTML = "Stats";
 		cell4.innerHTML = "";
-
-
 
 
 		let passed = Array.prototype.filter.call(document.querySelectorAll(
@@ -49,11 +83,17 @@ chrome.extension.sendMessage({}, function(response) {
 			'td.confluenceTd'),
 			(el) => (el.innerText.startsWith('Blocked')));
 		cell4.innerHTML = cell4.innerHTML + '<br>Blocked: '+ blocked.length;
-		
-		Array.prototype.forEach.call(blocked,
-			(el) => el.style['background-color'] = 'red' );
 
-        console.log("scripts/inject.js completed");
+
+    //TODO modularize me
+
+    columnsWithJiraInfo.forEach((info) => {
+      var cell = row.insertCell();
+      cell.innerHTML = 'JIRA links found:' + info.jIRAInfos.length;
+    });
+
+
+    console.log("scripts/inject.js completed");
 	}
 	}, 10);
 });
