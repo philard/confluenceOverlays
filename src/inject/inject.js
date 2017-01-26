d3f9daaf897fa12477f84bb1b32826e2eb6faa23
Philip John Ardley <Philip_John_Ardley@epam.com>
2017-01-26 17:48:17 +0800

    Bit refactor. JIRA status counting.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -18,79 +18,133 @@ chrome.extension.sendMessage({}, function(response) {
 
 
 
-		let testResultsHeader = document.querySelectorAll('thead div.tablesorter-header-inner');
-    testResultsHeader = Array.prototype.filter.call(testResultsHeader,
-      (el) => el.innerText.indexOf('Test Result'))[0];
 
-    let headers = Array.prototype.slice.call(
-      getClosest('tr', testResultsHeader).childNodes);
+    let countableTexts = ['Passed', 'Failed', 'Skipped', 'Blocked'];
+    let countableJIRAStatuses = ['ON HOLD', 'QA TEST', 'CLOSED', 'DELIVERED'];
 
+    // *All* other DOM links are relative to this starting anchor. ...
+    let testResultsHeader = Array.prototype.slice.call(
+      document.querySelectorAll('thead div.tablesorter-header-inner'))
+      .filter((td) => (td.innerText.indexOf('Test Result') == 0))
+      [0].parentNode;
+    // ... e.g. parentTable & statsRow
     let parentTable = getClosest('table', testResultsHeader);
+    let statsRow = parentTable.insertRow(1);
 
-    let jIRALinks = parentTable.querySelectorAll('a.jira-issue-key');
+
+    let jIRASpans = parentTable.querySelectorAll('span.aui-lozenge-success');
     let jIRAInfos = [];
-    for(i =0; jIRALinks.length > i; i++) {
-      let td = getClosest('td', jIRALinks[0]);
-      let trIndex = Array.prototype.indexOf.call(td.parentNode.childNodes, td);
+    for(i =0; jIRASpans.length > i; i++) {
+      let td = getClosest('td', jIRASpans[i]);
+      let yIndex = Array.prototype.slice.call(td.parentNode.childNodes).indexOf(td);
       jIRAInfo = {
-        'key': jIRALinks[0].attributes['href'].value,
-        'status': jIRALinks[0].innerText,
-        'trIndex': trIndex,
-        'headerEl': headers[trIndex]
+        // TODO 'key': jIRASpans[0].attributes['href'].value,
+        'status': jIRASpans[i].innerText,
+        'yIndex': yIndex
       };
       jIRAInfos.push(jIRAInfo);
     }
 
-    let columnsWithJiraInfo = headers.map((header, index) => {
-      return {
-        'index': index,
-        'headerEl': header,
-        'jIRAInfos': jIRAInfos.filter((info) => info.trIndex == index)
+    //headerInfos
+    let headerInfos = Array.prototype.slice.call(getClosest('tr', testResultsHeader).childNodes);
+    headerInfos = headerInfos.filter((el) => (el.nodeName == "TH"));
+    headerInfos = headerInfos.map((td, index) => {
+      let label = null;
+
+      //jIRAInfosForHeaderInfo
+      let jIRAInfosForHeaderInfo = jIRAInfos.filter((info) => info.yIndex == index)
+
+      //jIRAInfosForHeaderInfoForText
+      let jIRAInfosForHeaderInfoForText = {};
+
+
+      //tdEls
+      let tdEls = getTdsForHeaderIndex(index) || [];
+
+      //tdElsForText
+      let tdElsForText = {};
+
+      //label
+      if(index == 0) {
+        label = 'leftAxis';
+      } else if(td == testResultsHeader) {
+        label = 'testResult';
+
+        countableTexts.forEach((text) => {
+          let filtered = tdEls.filter((tdEl) => tdEl.innerText.indexOf(text) == 0);
+          tdElsForText[text] = filtered;
+        });
+
+      } else if(jIRAInfosForHeaderInfo.length > 0) {
+        label = 'JIRA';
+
+        countableJIRAStatuses.forEach((status) => {
+          let forStatus = jIRAInfosForHeaderInfo.filter((jIRAInfo) => {
+            return jIRAInfo.status == status;
+          });
+          jIRAInfosForHeaderInfoForText[status] = forStatus;
+        });
       }
+
+
+
+      return {
+        'label': label,
+        'index': index,
+        'headerEl': td,
+        'statsEl': statsEl = statsRow.insertCell(),
+        'tdEls': tdEls,
+        'tdElsForText': tdElsForText,
+        'jIRAInfos': jIRAInfosForHeaderInfo,
+        'jIRAInfosForText': jIRAInfosForHeaderInfoForText
+      };
     });
 
-    columnsWithJiraInfo =
-      columnsWithJiraInfo.filter((column) => column.jIRAInfos.length > 0);
 
+    headerInfos.forEach((headerInfo, index) => {
 
+    });
 
-		var row = parentTable.insertRow(1);
-		var cell1 = row.insertCell(0);
-		row.insertCell();
-		row.insertCell();
-		row.insertCell();
-		var cell4 = row.insertCell();
-		cell1.innerHTML = "Stats";
-		cell4.innerHTML = "";
+		// headerInfos[0].statsEl.innerHTML = "Stats";
 
+    function getTdsForHeaderIndex(header, selector) {
+      //TODO add extra filtering using selector
+      let headerIndex = (header.index || header);
+      let tds = parentTable.querySelectorAll('tbody > tr > td:nth-child(' + (headerIndex+1) + ')');
+      return (Array.prototype.slice.call(tds) || []);
+    }
 
-		let passed = Array.prototype.filter.call(document.querySelectorAll(
-			'td.confluenceTd'),
-			(el) => (el.innerText.startsWith('Passed')));
-		cell4.innerHTML = cell4.innerHTML + 'Passed:' + passed.length;
+    //updateStatsUi
+    headerInfos.forEach(updateStatsUi);
+    function updateStatsUi(headerInfo) {
+      switch(headerInfo.label) {
+        case "leftAxis":
+          headerInfo.statsEl.innerText = 'Stats'
+          break;
+        case "testResult":
+          countableTexts.forEach((text) => {
+            let stat = text + ': ' + headerInfo.tdElsForText[text].length + '<br>'
+            headerInfo.statsEl.innerHTML += stat;
+          });
+          break;
+        case "JIRA":
 
-		let failed = Array.prototype.filter.call(document.querySelectorAll(
-			'td.confluenceTd'),
-			(el) => (el.innerText.startsWith('Failed')));
-		cell4.innerHTML = cell4.innerHTML + '<br>Failed: '+ failed.length;
-
-		let skipped = Array.prototype.filter.call(document.querySelectorAll(
-			'td.confluenceTd'),
-			(el) => (el.innerText.startsWith('Skipped')));
-		cell4.innerHTML = cell4.innerHTML + '<br>Skipped: '+ skipped.length;
-
-		let blocked = Array.prototype.filter.call(document.querySelectorAll(
-			'td.confluenceTd'),
-			(el) => (el.innerText.startsWith('Blocked')));
-		cell4.innerHTML = cell4.innerHTML + '<br>Blocked: '+ blocked.length;
+          headerInfo.statsEl.innerHTML = '_JIRA statuses_<br>';
+          Object.keys(headerInfo.jIRAInfosForText).forEach((status) => {
+            let count = headerInfo.jIRAInfosForText[status].length;
+            headerInfo.statsEl.innerHTML += status + ': ' + count + '<br>';
+          });
+          break;
+      }
+    }
 
 
     //TODO modularize me
 
-    columnsWithJiraInfo.forEach((info) => {
-      var cell = row.insertCell();
-      cell.innerHTML = 'JIRA links found:' + info.jIRAInfos.length;
-    });
+    // columnsWithJiraInfo.forEach((info) => {
+    //   var cell = statsRow.insertCell();
+    //   cell.innerHTML = 'JIRA links found:' + info.jIRAInfos.length;
+    // });
 
 
     console.log("scripts/inject.js completed");
